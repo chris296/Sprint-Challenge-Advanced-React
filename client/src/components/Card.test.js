276675aa90e7c1f7ad190f33e37ef69f9b8336de
chris296 <chris.rios296@gmail.com>
2020-03-13 11:19:36 -0700

    third commit

--- client/src/components/Card.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Card from './Card';
-
-test('renders searches in card', () => {
-    const {getByText} = render(<Card/>);
-    getByText(/searches/i);
-})
